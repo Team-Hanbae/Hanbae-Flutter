36d2180103e04891c288b27b335ea8491d7cf458
Yunki-H <yunki.dev@gmail.com>
2025-03-04 22:05:27 +0900

    [refactor] #1 주석제거

--- lib/data/sound_manager.dart
+++ lib/data/sound_manager.dart
@@ -1,8 +1,6 @@
 import 'package:audioplayers/audioplayers.dart';
 
 class SoundManager {
-  // final AudioPlayer player = AudioPlayer();
-
   Future<void> play(String assetPath) async {
     final player = AudioPlayer();
     await player.play(AssetSource(assetPath));
