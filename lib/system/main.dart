b6342765487108b572a77b363ba475da24eea82f
Yunki-H <yunki.dev@gmail.com>
2025-03-05 16:04:42 +0900

    [feat] #1 기본테마 dark로 변경

--- lib/system/main.dart
+++ lib/system/main.dart
@@ -11,6 +11,7 @@ class Hanbae extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: HomeScreen(),
     );
   }
