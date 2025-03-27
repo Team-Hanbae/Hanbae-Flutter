499550f2bac594922f68038d622729d6beae38da
daydog1122 <haha4680@gmail.com>
2025-03-27 20:43:44 +0900

    [feat] #13 폰트크기와 패딩

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -10,87 +10,122 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: SizedBox(
-          width: 44,
-          height: 44,
-          child: Image.asset("assets/images/AppLogo.png"),
+        leading: Container(
+          margin: const EdgeInsets.only(left: 16),
+          child: SizedBox(
+            width: 44,
+            height: 44,
+            child: Image.asset("assets/images/AppLogo.png"),
+          ),
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("내가 저장한 장단"),
-                    TextButton(onPressed: () {}, child: Text("더보기")),
-                  ],
-                ),
-                SizedBox(
-                  height: 84,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 10,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        width: 156,
-                        height: 84,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.all(Radius.circular(16)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "내가 저장한 장단",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
                         ),
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [Text("흥부가 돈타령"), Text("중중모리")],
-                          ),
-                        ),
-                      );
-                    },
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(minimumSize: Size(0, 40)),
+                        child: Text("더보기", style: TextStyle(fontSize: 16)),
+                      ),
+                    ],
                   ),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Row(children: [Text("바로 연습하기")]),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  itemCount: 20,
-                  itemBuilder: (content, index) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            SizedBox(
-                              width: 64,
-                              height: 64,
-                              child: Center(
-                                child: SizedBox(
-                                  width: 36,
-                                  height: 36,
-                                  child: SvgPicture.asset(
-                                    "assets/images/logos/Jinyang.svg",
+
+                  SizedBox(height: 16),
+
+                  SizedBox(
+                    height: 84,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: 156,
+                          height: 84,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.all(Radius.circular(16)),
+                          ),
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [Text("흥부가 돈타령"), Text("중중모리")],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 32),
+
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "바로 연습하기",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 16),
+
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    itemCount: 20,
+                    itemBuilder: (content, index) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 64,
+                                height: 64,
+                                child: Center(
+                                  child: SizedBox(
+                                    width: 36,
+                                    height: 36,
+                                    child: SvgPicture.asset(
+                                      "assets/images/logos/Jinyang.svg",
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Column(children: [Text("진양"), Text("24박 3소박")]),
-                          ],
-                        ),
-                        Icon(Icons.chevron_right_rounded),
-                      ],
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
+                              Column(children: [Text("진양"), Text("24박 3소박")]),
+                            ],
+                          ),
+                          Icon(Icons.chevron_right_rounded),
+                        ],
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
