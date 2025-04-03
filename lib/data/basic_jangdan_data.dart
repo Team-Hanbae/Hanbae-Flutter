72b3db01e8c403c6fc54eeedc6c546bb7a2f8f46
daydog1122 <haha4680@gmail.com>
2025-04-03 21:27:58 +0900

    [feat] #13 basic장단 임시데이터 만듦

---
+++ lib/data/basic_jangdan_data.dart
@@ -0,0 +1,21 @@
+class BasicJangdanItem {
+  final String name;
+  final String bakType;
+
+  BasicJangdanItem({required this.name, required this.bakType});
+}
+
+final List<BasicJangdanItem> basicJangdanList = [
+  BasicJangdanItem(name: "진양", bakType: "24박 3소박"),
+  BasicJangdanItem(name: "중중모리", bakType: "12박 2소박"),
+  BasicJangdanItem(name: "중모리", bakType: "4박 3소박"),
+  BasicJangdanItem(name: "자진모리", bakType: "4박 3소박"),
+  BasicJangdanItem(name: "굿거리", bakType: "4박 3소박"),
+  BasicJangdanItem(name: "동살풀이", bakType: "4박 2소박"),
+  BasicJangdanItem(name: "세마치", bakType: "3박 3소박"),
+  BasicJangdanItem(name: "엇모리", bakType: "4박 3+2소박"),
+  BasicJangdanItem(name: "엇중모리", bakType: "6박 2소박"),
+  BasicJangdanItem(name: "휘모리", bakType: "4박 2소박"),
+  BasicJangdanItem(name: "긴염불", bakType: "6박 3소박"),
+  BasicJangdanItem(name: "반염불", bakType: "6박 3소박")
+];
