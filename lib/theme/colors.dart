68de6ef7b62a94f6451570ace58427055252d1c4
daydog1122 <haha4680@gmail.com>
2025-04-05 00:09:42 +0900

    [feat] #15 컬러 등록

---
+++ lib/theme/colors.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class ColorPrimitives {
+  static const Color blackBlack1 = Color(0xFFFFFFFF);
+  static const Color orangeOrange50 = Color(0xFFFFF5EA);
+  static const Color gradientTop = Color(0xFFFF823B);
+  static const Color gradientBottom = Color(0xFFFF9C31);
+  static const Color orangeOrange100 = Color(0xFFFFE0BF);
+  static const Color orangeOrange200 = Color(0xFFFFD1A0);
+  static const Color orangeOrange300 = Color(0xFFFFBD75);
+  static const Color orangeOrange400 = Color(0xFFFFB05A);
+  static const Color orangeOrange500 = Color(0xFFFF9C31);
+  static const Color orangeOrange600 = Color(0xFFE88E2D);
+  static const Color orangeOrange700 = Color(0xFFB56F23);
+  static const Color orangeOrange800 = Color(0xFF8C561B);
+  static const Color orangeOrange900 = Color(0xFF6B4215);
+  static const Color orangeOrange1300 = Color(0xFF321D07);
+  static const Color etcBlink = Color(0xFFFF942F);
+  static const Color blackBlack2 = Color(0xFFFCFCFC);
+  static const Color blackBlack3 = Color(0xFFF5F5F5);
+  static const Color blackBlack4 = Color(0xFFF0F0F0);
+  static const Color blackBlack5 = Color(0xFFD9D9D9);
+  static const Color blackBlack6 = Color(0xFFBFBFBF);
+  static const Color blackBlack7 = Color(0xFF8C8C8C);
+  static const Color blackBlack8 = Color(0xFF595959);
+  static const Color blackBlack9 = Color(0xFF454545);
+  static const Color blackBlack10 = Color(0xFF303030);
+  static const Color blackBlack11 = Color(0xFF1F1F1F);
+  static const Color blackBlack12 = Color(0xFF141414);
+  static const Color blackBlack13 = Color(0xFF000000);
+}
+
+  class AppColors {
+  static Color backgroundDefault = ColorPrimitives.blackBlack13;
+  static Color backgroundCard = ColorPrimitives.blackBlack11;
+  static Color backgroundSheet = ColorPrimitives.blackBlack12;
+  static Color backgroundBackgroundNavigationbar = ColorPrimitives.blackBlack11;
+  static Color backgroundImageDefault = ColorPrimitives.blackBlack10;
+  static Color backgroundImageActive = ColorPrimitives.orangeOrange400;
+
+  static Color buttonPrimary = ColorPrimitives.blackBlack8;
+  static Color buttonBpmControlDefault = ColorPrimitives.blackBlack10;
+  static Color buttonBpmControlActive = ColorPrimitives.blackBlack8;
+  static Color buttonToggleOff = ColorPrimitives.blackBlack9;
+  static Color buttonToggleOn = ColorPrimitives.orangeOrange600;
+  static Color buttonToggleKnobInactive = ColorPrimitives.blackBlack8;
+  static Color buttonPlaystart = ColorPrimitives.blackBlack1;
+  static Color buttonPlaystop = ColorPrimitives.blackBlack8;
+  static Color buttonActive = ColorPrimitives.orangeOrange500;
+  static Color buttonReverse = ColorPrimitives.blackBlack1;
+  static Color buttonCancel = ColorPrimitives.blackBlack8;
+
+  static Color bakBarActiveTop = ColorPrimitives.gradientTop;
+  static Color bakBarActiveBottom = ColorPrimitives.gradientBottom;
+  static Color bakBarInactive = ColorPrimitives.blackBlack9;
+  static Color bakBarLine = ColorPrimitives.blackBlack13;
+  static Color bakBarBorder = ColorPrimitives.blackBlack8;
+  static Color frame = ColorPrimitives.blackBlack11;
+  static Color bakBarNumberBlack = ColorPrimitives.blackBlack11;
+  static Color bakBarNumberWhite = ColorPrimitives.blackBlack1;
+  static Color bakBarNumberGray = ColorPrimitives.blackBlack9;
+  static Color bub = ColorPrimitives.blackBlack3;
+  static Color sobakSegmentDaebak = ColorPrimitives.orangeOrange500;
+  static Color sobakSegmentSobak = ColorPrimitives.orangeOrange300;
+  static Color bakBarIndicator = ColorPrimitives.orangeOrange500;
+  static Color bakBarDivider = ColorPrimitives.blackBlack8;
+
+  static Color textDefault = ColorPrimitives.blackBlack1;
+  static Color textSecondary = ColorPrimitives.blackBlack3;
+  static Color textTertiary = ColorPrimitives.blackBlack6;
+  static Color textQuaternary = ColorPrimitives.blackBlack7;
+  static Color textBPMDefault = ColorPrimitives.blackBlack4;
+  static Color textButtonPrimary = ColorPrimitives.blackBlack1;
+  static Color textButtonSecondary = ColorPrimitives.blackBlack2;
+  static Color textButtonCancel = ColorPrimitives.blackBlack6;
+  static Color textButtonEmphasis = ColorPrimitives.blackBlack11;
+  static Color textBPMSearch = ColorPrimitives.orangeOrange600;
+
+  static Color progressPercentage = ColorPrimitives.orangeOrange500;
+  static Color progressFrame = ColorPrimitives.blackBlack8;
+  static Color blink = ColorPrimitives.etcBlink;
+  static Color jangdanLogoBackground = ColorPrimitives.orangeOrange1300;
+  static Color jangdanLogoPrimary = ColorPrimitives.orangeOrange700;
+  }
