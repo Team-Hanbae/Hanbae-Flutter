62ded21e12b182f4a36c25df61bf106f3769df8c
daydog1122 <haha4680@gmail.com>
2025-04-05 22:01:52 +0900

    [feat] #19 main theme dark에 배경색만 오버라이드

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hanbae/presentation/home/home_screen.dart';
+import 'package:hanbae/theme/colors.dart';
 
 void main() {
   runApp(Hanbae());
@@ -12,7 +13,11 @@ class Hanbae extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark().copyWith(
+        // ThemeData.dark()를 유지하면서
+        scaffoldBackgroundColor:
+            AppColors.backgroundDefault, // scaffold 배경색을 오버라이드
+      ),
       home: HomeScreen(),
     );
   }
