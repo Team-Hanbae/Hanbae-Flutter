57108155c5873467cc54086619757a50441caa41
Yunki-H <yunki.dev@gmail.com>
2025-12-01 21:19:05 +0900

    [feat] #166 최근 연습 목록

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -106,213 +106,161 @@ class HomeScreen extends StatelessWidget {
 
             const SizedBox(height: 24),
 
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "내가 저장한 장단",
+            // 최근 연습
+            if (recentPlayedJangdanList.isNotEmpty) ...[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsGeometry.only(left: 8),
+                      child: Text(
+                        "최근 연습",
                         style: AppTextStyles.title2B.copyWith(
                           color: AppColors.labelPrimary,
                         ),
                       ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => CustomJangdanListScreen(),
-                            ),
-                          );
-                        },
-                        style: TextButton.styleFrom(
-                          minimumSize: Size(40, 40),
-                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                          padding: EdgeInsets.zero,
-                        ),
-                        child: Text(
-                          "더보기",
-                          style: AppTextStyles.calloutR.copyWith(
-                            color: AppColors.labelSecondary,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                    ),
 
-                const SizedBox(height: 8),
+                    const SizedBox(height: 16),
 
-                SizedBox(
-                  height: 84,
-                  child:
-                      customJangdanList.isEmpty
-                          ? Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
+                    Row(
+                      spacing: 8,
+                      children: List.generate(3, (index) {
+                        if (index >= recentPlayedJangdanList.length) {
+                          return const Expanded(child: SizedBox());
+                        }
+
+                        final jangdan = recentPlayedJangdanList[index];
+                        final isCustomJangdan =
+                            jangdan.name != jangdan.jangdanType.label;
+
+                        return Expanded(
+                          child: InkWell(
+                            onTap: () {
+                              context.read<MetronomeBloc>().add(
+                                SelectJangdan(jangdan),
+                              );
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder:
+                                      (context) => MetronomeScreen(
+                                        jangdan: jangdan,
+                                        appBarMode: AppBarMode.custom,
+                                      ),
+                                ),
+                              );
+                            },
                             child: Container(
-                              width: double.infinity,
+                              height: 100,
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              clipBehavior: Clip.hardEdge,
                               decoration: BoxDecoration(
                                 color: AppColors.backgroundMute,
                                 borderRadius: BorderRadius.all(
                                   Radius.circular(16),
                                 ),
                               ),
-                              child: Center(
-                                child: Text(
-                                  "저장한 장단이 없어요",
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: AppColors.labelTertiary,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          )
-                          : ListView.separated(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            scrollDirection: Axis.horizontal,
-                            itemCount:
-                                customJangdanList.length >= 2
-                                    ? customJangdanList.length + 1
-                                    : customJangdanList.length,
-                            separatorBuilder:
-                                (context, index) => SizedBox(width: 8),
-                            itemBuilder: (context, index) {
-                              if (customJangdanList.length >= 2 &&
-                                  index == customJangdanList.length) {
-                                return InkWell(
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder:
-                                            (context) =>
-                                                CustomJangdanListScreen(),
-                                      ),
-                                    );
-                                  },
-                                  child: Container(
-                                    width: 156,
-                                    height: 84,
-                                    decoration: BoxDecoration(
-                                      color: AppColors.backgroundMute,
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(16),
-                                      ),
-                                    ),
-                                    child: Center(
-                                      child: Text(
-                                        "더보기",
-                                        style: TextStyle(
-                                          fontSize: 17,
-                                          color: AppColors.labelTertiary,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              } else {
-                                final item = customJangdanList[index];
-                                return InkWell(
-                                  onTap: () {
-                                    context.read<MetronomeBloc>().add(
-                                      SelectJangdan(item),
-                                    );
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder:
-                                            (context) => MetronomeScreen(
-                                              jangdan: item,
-                                              appBarMode: AppBarMode.custom,
-                                            ),
-                                      ),
-                                    );
-                                  },
-                                  child: Container(
-                                    width: 156,
-                                    height: 84,
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 16,
-                                    ),
-                                    clipBehavior: Clip.hardEdge,
-                                    decoration: BoxDecoration(
-                                      color: AppColors.backgroundMute,
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(16),
-                                      ),
-                                    ),
-                                    child: Stack(
-                                      alignment: Alignment.center,
-                                      children: [
-                                        Positioned.fill(
-                                          child: OverflowBox(
-                                            maxWidth: double.infinity,
-                                            maxHeight: double.infinity,
-                                            child: Transform.translate(
-                                              offset: Offset(0, 110),
-                                              child: Container(
-                                                width: 300,
-                                                height: 300,
-                                                decoration: BoxDecoration(
-                                                  shape: BoxShape.circle,
-                                                  gradient: RadialGradient(
-                                                    radius: 1.0,
-                                                    center: Alignment.center,
-                                                    colors: [
-                                                      AppColors.bakBarTop,
-                                                      AppColors.neutral12
-                                                          .withAlpha(0),
-                                                    ],
-                                                    stops: [0, 0.5],
-                                                  ),
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Positioned.fill(
+                                    child: OverflowBox(
+                                      maxWidth: double.infinity,
+                                      maxHeight: double.infinity,
+                                      child: Transform.translate(
+                                        offset: Offset(0, 110),
+                                        child: Container(
+                                          width: 300,
+                                          height: 300,
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            gradient: RadialGradient(
+                                              radius: 1.0,
+                                              center: Alignment.center,
+                                              colors: [
+                                                AppColors.bakBarTop,
+                                                AppColors.neutral12.withAlpha(
+                                                  0,
                                                 ),
-                                              ),
+                                              ],
+                                              stops: [0, 0.5],
                                             ),
                                           ),
                                         ),
-                                        Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          spacing: 2,
-                                          children: [
-                                            Text(
-                                              item.name,
-                                              maxLines: 1,
-                                              overflow: TextOverflow.ellipsis,
-                                              style: AppTextStyles.headlineSb
-                                                  .copyWith(
-                                                    color:
-                                                        AppColors.labelDefault,
-                                                  ),
-                                            ),
-                                            Text(
-                                              item.jangdanType.label,
-                                              style: AppTextStyles.subheadlineR
-                                                  .copyWith(
-                                                    color:
-                                                        AppColors
-                                                            .labelSecondary,
-                                                  ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
+                                      ),
                                     ),
                                   ),
-                                );
-                              }
-                            },
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    spacing: isCustomJangdan ? 4 : 8,
+                                    children: [
+                                      isCustomJangdan
+                                          ? Text(
+                                            jangdan.name,
+                                            maxLines: 1,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: AppTextStyles.calloutSb
+                                                .copyWith(
+                                                  color: AppColors.labelDefault,
+                                                ),
+                                          )
+                                          : SizedBox(
+                                            width: 36,
+                                            height: 36,
+                                            child: SvgPicture.asset(
+                                              "assets/${jangdan.jangdanType.logoAssetPath}",
+                                              colorFilter: ColorFilter.mode(
+                                                AppColors.orange8,
+                                                BlendMode.srcIn,
+                                              ),
+                                            ),
+                                          ),
+                                      Text(
+                                        jangdan.jangdanType.label,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style:
+                                            isCustomJangdan
+                                                ? AppTextStyles.subheadlineR
+                                                    .copyWith(
+                                                      color:
+                                                          AppColors
+                                                              .labelSecondary,
+                                                    )
+                                                : AppTextStyles.subheadlineSb
+                                                    .copyWith(
+                                                      color:
+                                                          AppColors
+                                                              .labelDefault,
+                                                    ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
+                        );
+                      }),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+
+              const SizedBox(height: 32),
+
+              Container(
+                width: double.infinity,
+                height: 10,
+                color: AppColors.backgroundDark,
+              ),
+            ],
 
             //내가 저장한 장단 끝
-            const SizedBox(height: 32),
+            const SizedBox(height: 24),
 
             // 장단 카테고리
             Column(
